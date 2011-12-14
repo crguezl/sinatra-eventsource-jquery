021018f5f5ef8347a6dba4888e45ada3ac1247de
Konstantin Haase <konstantin.mailinglists@googlemail.com>
2011-12-14 12:18:48 +0100

    use EventSource

--- app.rb
+++ app.rb
@@ -7,9 +7,10 @@ get '/' do
 end
 
 get '/bottles' do
+  content_type "text/event-stream"
   stream do |out|
     1000.times do |i|
-      out << "#{i} bottle(s) on a wall...\n"
+      out << "data: #{i} bottle(s) on a wall...\n\n"
       sleep 0.5
     end
   end
